e64760c930d7596a95ca639ee74c693fff520df2
Matthias Nehlsen <mn@nehlsen-edv.de>
2013-03-23 16:52:06 +0100

    conf.get throws error when conf key not found

--- app/utils/Conf.scala
+++ app/utils/Conf.scala
@@ -4,6 +4,10 @@ import play.api.Play.current
 
 object Conf {
   
-  def get(key: String) = current.configuration.getString(key).getOrElse("")
+  /** Retrieves a key from the application.conf. Throws exception if key not found.
+   *  @param    key String configuration key
+   *  @return   String with configuration key
+   */
+  def get(key: String) = current.configuration.getString(key).getOrElse(throw new Exception("Couldn't find config for " + key))
 
 }

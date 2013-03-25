839dd29a10b9bb7b4316a6bb64f60d46eb3cd6d6
Matthias Nehlsen <mn@nehlsen-edv.de>
2013-03-25 17:51:08 +0100

    import cleanup

--- app/controllers/Images.scala
+++ app/controllers/Images.scala
@@ -1,29 +1,12 @@
 package controllers
 
 import play.api.mvc.{ Action, Controller }
-import play.api.mvc.{ Action, Controller, WebSocket }
-import play.api.mvc.SimpleResult
-import play.api.mvc.ResponseHeader
-import play.api.libs.iteratee._
-import play.api.libs.json._
-//import play.api.libs.concurrent.Execution.Implicits._
-import org.joda.time.DateTime
 
 import play.modules.reactivemongo._
-import reactivemongo.api._
 import reactivemongo.bson._
-import reactivemongo.bson.BSONValue
-import reactivemongo.bson.handlers._
-import reactivemongo.api.gridfs._
 import reactivemongo.bson.handlers.DefaultBSONHandlers.DefaultBSONDocumentWriter
-import reactivemongo.bson.handlers.DefaultBSONHandlers.DefaultBSONReaderHandler
-
-import akka.actor.{ Actor, ActorSystem, DeadLetter, Props }
 
 import utils._
-import models._
-import akka._
-import models.TweetImplicits._
 
 /** Controller for serving locally cached twitter profile images */
 object Images extends Controller with MongoController{

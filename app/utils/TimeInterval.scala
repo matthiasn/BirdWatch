b66c5e0a953e137b2c6c3ee36c0007adf3801e9b
Matthias Nehlsen <mn@nehlsen-edv.de>
2013-04-09 14:17:59 +0200

    moving TimeInterval to core project

---
+++ app/utils/TimeInterval.scala
@@ -0,0 +1,57 @@
+package utils
+
+import scala.collection.immutable.ListMap
+
+case class TimeInterval(millis: Long, intervalMap: ListMap[String, Int]) {
+
+  override def toString: String = toString(2)
+
+  def toFullString: String = toString(intervalMap.size)
+
+  def toString(n: Int): String = {
+    def formatKeyValue(key: String, value: Int): String = s"$value $key${if(value>1 && key !="ms") "s" else ""} "
+
+    (intervalMap.dropWhile(_._2 == 0) // drops leading elements with v == 0
+      .take(n)              // takes next n elements
+      .filter(_._2 != 0)    // leaves only the elements with v != 0
+      .foldLeft("") { case (res, (k, v)) => res + formatKeyValue(k, v)} trim) + " ago"
+  }
+}
+
+object TimeInterval {
+
+  /**
+   * Constructing tIntervals ListMap with time intervals.
+   * The choice of ListMap preverves insertion order, which allows
+   * to right fold over the members in the map in the order
+   * year, week, day, ...
+   */
+  val ms = ListMap("ms" -> 1L)
+  val sec = ms ++ ListMap("sec" -> ms("ms") * 1000)
+  val min = sec ++ ListMap("min" -> sec("sec") * 60)
+  val hour = min ++ ListMap("hour" -> min("min") * 60)
+  val day = hour ++ ListMap("day" -> hour("hour") * 24)
+  val week = day ++ ListMap("week" -> day("day") * 7)
+  val tIntervals = week ++ ListMap("year" -> day("day") * 365)
+
+  /**
+   * Function: interval
+   * Usage: TimeInterval.interval(millis)
+   * -----------------------------------------------
+   * Creates TimeInterval from milliseconds. 
+   * The intervalMap is created by right folding the intervalsInMillis Map
+   * (Order: year, week, day, ...) and putting the result of dividing the
+   * millis in the accumulator tuple by the current interval.
+   * For each subsequent fold, the accumulator tuple consist of the remainder
+   * of the division, and the map with the results (e.g.Map(year -> 1, week -> 2). 
+   * This map in the accumulator tuple is then used in the resulting TimeInterval.
+   *
+   * (cName, cval): name and value of current tIntervals element
+   * (rMs, res): accumulator with remaining millis, result map 
+   */
+  def apply(ms: Long): TimeInterval =
+    TimeInterval(ms, tIntervals.foldRight(ms, new ListMap[String, Int]()) {
+      case ((cName, cVal), (rMs, res)) =>
+        (rMs % cVal, res + (cName -> (rMs / cVal).toInt))
+    }._2) // Example: TimeInterval(3660010, ListMap( ..., hour -> 1, min -> 1, sec -> 0, ms -> 10)      
+}

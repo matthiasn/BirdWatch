81cd1a94e6a34bbabefd523770752ce093d94ac8
Matthias Nehlsen <mn@nehlsen-edv.de>
2013-04-11 21:34:04 +0200

    search string

--- app/Global.scala
+++ app/Global.scala
@@ -4,7 +4,7 @@ import utils.Mongo
 
 object Global extends GlobalSettings {
 
-  override def onStart(application: play.api.Application) { Tweet.listen("obama") }   // separate search words by "%2C"
+  override def onStart(application: play.api.Application) { Tweet.listen("amsterdam%2Chamburg") }   // separate search words by "%2C"
     
   override def onStop(application: play.api.Application) { Mongo.connection.close() }
 }

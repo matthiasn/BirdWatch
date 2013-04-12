33b9248e7b6cf65a910d260e59c6d80caa2ecf85
Matthias Nehlsen <mn@nehlsen-edv.de>
2013-04-12 11:08:13 +0200

    code formatting

--- app/controllers/Images.scala
+++ app/controllers/Images.scala
@@ -1,27 +1,27 @@
 package controllers
 
-import play.api.mvc.{ Action, Controller }
+import play.api.mvc.{Action, Controller}
 import play.modules.reactivemongo.MongoController
 
 import reactivemongo.api.gridfs.Implicits.DefaultReadFileReader
-import reactivemongo.bson.{ BSONString, BSONDocument }
+import reactivemongo.bson.{BSONString, BSONDocument}
 import reactivemongo.bson.handlers.DefaultBSONHandlers.DefaultBSONDocumentWriter
 
 import utils._
 
 /** Controller for serving locally cached twitter profile images */
-object Images extends Controller with MongoController{
- 
+object Images extends Controller with MongoController {
+
  /** Serves images connection updating the UI 
-  *  CAUTION: reactivemongo marks file CONTENT_DISPOSITION as attachment (why???) 
-  *  @param filename name of the image to serve
+  * CAUTION: reactivemongo marks file CONTENT_DISPOSITION as attachment (why???) 
+  * @param filename name of the image to serve
   */
   def getImage(filename: String) = Action {
-    Async {      
+    Async {
       // finds the matching attachment, if any, and streams it to the client
       val file = Mongo.imagesGridFS.find(BSONDocument("filename" -> new BSONString(filename)))
       serve(Mongo.imagesGridFS, file)
     }
   }
-  
+
 }

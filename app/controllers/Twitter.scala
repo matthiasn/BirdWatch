27ca9552391f905e4a3567d2471006acc21ac8aa
Matthias Nehlsen <mn@nehlsen-edv.de>
2013-04-12 11:07:54 +0200

    code formatting, comments

--- app/controllers/Twitter.scala
+++ app/controllers/Twitter.scala
@@ -1,13 +1,13 @@
 package controllers
 
-import akka.actor.{ Actor, Props }
+import akka.actor.{Actor, Props}
 
 import org.joda.time.DateTime
 
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
-import play.api.libs.iteratee.{ Iteratee, Concurrent }
+import play.api.libs.iteratee.{Iteratee, Concurrent}
 import play.api.libs.json.Json
-import play.api.mvc.{ Action, Controller, WebSocket }
+import play.api.mvc.{Action, Controller, WebSocket}
 
 import reactivemongo.api._
 import reactivemongo.bson._
@@ -22,118 +22,125 @@ import utils._
 import models.TweetState
 import scala.concurrent.Future
 
-/** Controller for serving main BirdWatch page including the websocket connection */
+/** Controller for serving main BirdWatch page including the WebSocket connection */
 object Twitter extends Controller {
 
-  /** Serves HTML page (static content at the moment, page gets updates through websocket) */
-  def tweetList() = Action { 
+  /** Serves HTML page (static content at the moment, page gets updates through WebSocket) */
+  def tweetList() = Action {
     implicit request => {
-      RequestLogger.log(request)     
+      RequestLogger.log(request)
       Ok(views.html.twitter.tweets(Seq[Tweet]()))
-    } 
+    }
   }
 
   /** Serves WebSocket connection updating the UI */
-  def tweetFeed() = WebSocket.using[String] { implicit request =>
-    
-    /** Iteratee for incoming messages on websocket connection, currently ignored */
-    val in = Iteratee.ignore[String]
-    
-   /** Creates enumerator and channel for Strings through Concurrent factory object
-    *  for pushing data through the websocket */
-    val (out, wsOutChannel) = Concurrent.broadcast[String]
-    
-   /** "side-effecting" function to do something with the accumulator without possibly mutating it
-    *  e.g. push some computation to a websocket enumerator or to log file
-    *  @param    tweetList accumulator inside the Iteratee
-    *  @return   Unit, cannot interfere with the accumulator inside the Iteratee 
-    */
-    def interceptTweetList(tweetList: List[Tweet]) {
-      
-      val (charCountMean, charCountStdDev) = Calc.stdDev(tweetList.map(t => t.charCount))
-      val (wordCountMean, wordCountStdDev) = Calc.stdDev(tweetList.map(t => t.wordCount))
-      
-      val tweetState = TweetState(tweetList.take(50), WordCount.topN(tweetList, 250), charCountMean, charCountStdDev, 
-        wordCountMean, wordCountStdDev, tweetList.size)
-      
-     wsOutChannel.push(Json.stringify(Json.toJson(tweetState)))      
-    }
-    
-   /** Creates enumerator and channel for Tweets through Concurrent factory object */
-    val (enumerator, tweetChannel) = Concurrent.broadcast[Tweet]
-    
-    /** Iteratee processing Tweets from tweetChannel, accumulating a rolling window of tweets */
-    val tweetListIteratee = WordCount.tweetListIteratee(interceptTweetList, List[Tweet](), 1000)
-    enumerator |>>> tweetListIteratee // attach tweetListIteratee to enumerator
+  def tweetFeed() = WebSocket.using[String] {
+    implicit request =>
 
-   /** Actor for subscribing to eventStream. Pushes received tweets into TweetChannel for
-    *  consumption through iteratee (and potentially other consumers, decoupled)  */
-    val subscriber = ActorStage.system.actorOf(Props(new Actor {
-      def receive = {
-        case t: Tweet => tweetChannel.push(t) // push received tweet into Concurrent.Channel[Tweet] 
+     /** Iteratee for incoming messages on WebSocket connection, currently ignored */
+      val in = Iteratee.ignore[String]
+
+     /** Creates enumerator and channel for Strings through Concurrent factory object
+      * for pushing data through the WebSocket */
+      val (out, wsOutChannel) = Concurrent.broadcast[String]
+
+     /** "side-effecting" function to do something with the accumulator without possibly mutating it
+      * e.g. push some computation to a WebSocket enumerator or to log file
+      * @param    tweetList accumulator inside the Iteratee
+      * @return   Unit, cannot interfere with the accumulator inside the Iteratee 
+      */
+      def interceptTweetList(tweetList: List[Tweet]) {
+
+        val (charCountMean, charCountStdDev) = Calc.stdDev(tweetList.map(t => t.charCount))
+        val (wordCountMean, wordCountStdDev) = Calc.stdDev(tweetList.map(t => t.wordCount))
+
+        val tweetState = TweetState(tweetList.take(50), WordCount.topN(tweetList, 250), charCountMean, charCountStdDev,
+          wordCountMean, wordCountStdDev, tweetList.size)
+
+        wsOutChannel.push(Json.stringify(Json.toJson(tweetState)))
       }
-    }))
-    
-    /** Pre-load the last 500 tweets through WebSocket connection  */
-    latestTweetQuery.map { 
-      tweets => tweets.take(500).reverse.foreach(t => tweetChannel.push(t))  // push last 500 tweets
-      ActorStage.system.eventStream.subscribe(subscriber, classOf[Tweet])    // subscribe to incoming tweets
-    }
 
-    (in, out) // in and out channels for WebSocket connection
+     /** Creates enumerator and channel for Tweets through Concurrent factory object */
+      val (enumerator, tweetChannel) = Concurrent.broadcast[Tweet]
+
+     /** Iteratee processing Tweets from tweetChannel, accumulating a rolling window of tweets */
+      val tweetListIteratee = WordCount.tweetListIteratee(interceptTweetList, List[Tweet](), 1000)
+      enumerator |>>> tweetListIteratee // attach tweetListIteratee to enumerator
+
+     /** Actor for subscribing to eventStream. Pushes received tweets into TweetChannel for
+      * consumption through iteratee (and potentially other consumers, decoupled)  */
+      val subscriber = ActorStage.system.actorOf(Props(new Actor {
+        def receive = {
+          case t: Tweet => tweetChannel.push(t) // push received tweet into Concurrent.Channel[Tweet] 
+        }
+      }))
+
+     /** Pre-load the last 500 tweets through WebSocket connection  */
+      latestTweetQuery.map {
+        tweets => tweets.take(500).reverse.foreach(t => tweetChannel.push(t)) // push last 500 tweets
+        ActorStage.system.eventStream.subscribe(subscriber, classOf[Tweet]) // subscribe to incoming tweets
+      }
+
+      (in, out) // in and out channels for WebSocket connection
   }
-  
-  def latestTweetQuery(): Future[List[Tweet]] = {
+
+
+ /** Query latest tweets (lazily evaluated stream, result could be od arbitrary size) */
+  def latestTweetQuery: Future[List[Tweet]] = {
     val query = QueryBuilder().query(BSONDocument("created_at" -> BSONDocument("$lte" -> BSONDateTime(DateTime.now.getMillis))))
       .sort("created_at" -> SortOrder.Descending)
 
     // run this query over the collection
     val cursor = Mongo.tweets.find(query)
-
-    // got the list of documents (in a fully non-blocking way)
+   
     cursor.toList
   }
-  
+
  /** Controller Action serving Tweets as JSON going backwards in time from the 
-  *  specified time in milliseconds from epoch
-  *  @param    millis time in millis
-  *  @param    results number of results to return 
-  */  
-  def tweetsJson(millis: Long, results: Int) = Action { implicit request => 
-    Async {      
-      latestTweetQuery.map { tweets => Ok(Json.toJson(tweets.take(results))) }     
-    }
+  * specified time in milliseconds from epoch
+  * @param    millis time in millis
+  * @param    results number of results to return 
+  */
+  def tweetsJson(millis: Long, results: Int) = Action {
+    implicit request =>
+      Async {
+        latestTweetQuery.map {
+          tweets => Ok(content = Json.toJson(tweets.take(results)))
+        }
+      }
   }
-  
+
  /** Controller Action serving Tweets as JSON going backwards in time from when
-  *  the action is called
-  *  @param    results number of results to return 
+  * the action is called
+  * @param    results number of results to return 
   */
   def tweetsJsonLatest(results: Int) = tweetsJson(DateTime.now.getMillis, results)
-  
-  /** Controller Action replaying the specified number of tweets from 
-   *  the specified time in millis forward.
-   *  @param    minutesAgo time in minutes
-   *  @param    results number of results to return 
-   */  
-   def tweetReplay(minutesAgo: Long, results: Int) = Action { implicit request => 
-     Async {      
-       val query = QueryBuilder().query(BSONDocument("created_at" -> 
-         BSONDocument("$gte" -> BSONDateTime(DateTime.now.getMillis - (minutesAgo * 60 * 1000)))))
-         .sort("created_at" -> SortOrder.Ascending)
 
-       // run this query over the collection
-       val cursor = Mongo.tweets.find(query)
+ /** Controller Action replaying the specified number of tweets from 
+  * the specified time in millis forward.
+  * @param    minutesAgo time in minutes
+  * @param    results number of results to return 
+  */
+  def tweetReplay(minutesAgo: Long, results: Int) = Action {
+    implicit request =>
+      Async {
+        val query = QueryBuilder().query(BSONDocument("created_at" ->
+          BSONDocument("$gte" -> BSONDateTime(DateTime.now.getMillis - (minutesAgo * 60 * 1000)))))
+          .sort("created_at" -> SortOrder.Ascending)
 
-       // got the list of documents (in a fully non-blocking way)
-       cursor.toList.map { tweets =>
-         tweets.take(results).foreach { 
-           t => ActorStage.system.eventStream.publish(t) 
-           Thread.sleep(250)
-         }
-         Ok(Json.toJson(tweets.take(results)))
-       }     
-     }
-   }
-  
+        // run this query over the collection
+        val cursor = Mongo.tweets.find(query)
+
+        // got the list of documents (in a fully non-blocking way)
+        cursor.toList.map {
+          tweets =>
+            tweets.take(results).foreach {
+              t => ActorStage.system.eventStream.publish(t)
+              Thread.sleep(250)
+            }
+            Ok(Json.toJson(tweets.take(results)))
+        }
+      }
+  }
+
 }

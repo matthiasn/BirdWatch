856cde3cd897022337abe763bd20fc443450d2be
Matthias Nehlsen <mn@nehlsen-edv.de>
2013-04-27 15:52:31 +0200

    removing  collection for Tweet case class

--- app/utils/Mongo.scala
+++ app/utils/Mongo.scala
@@ -1,31 +1,24 @@
 package birdwatchUtils
 
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.libs.json.JsValue
 
 import reactivemongo.api.MongoDriver
 import reactivemongo.api.gridfs.GridFS
-import play.api.libs.json.JsValue
-
 import play.modules.reactivemongo.json.collection.JSONCollection
 
 /** Mongo connection object */
 object Mongo {
 
+  /** Connection to BirdWatch database inMongoDB */
   val driver = new MongoDriver
-
-  /** Connection to MongoDB */
   val connection = driver.connection(List("localhost:27017"))
-  
-  /** Representation of BirdWatch database in MongoDB */
   val db = connection("BirdWatch")
-  
-  /** Representation of tweets collection in BirdWatch database */
-  def tweets: JSONCollection = db.collection[JSONCollection]("tweets")
-
-  def accessLog: JSONCollection = db.collection[JSONCollection]("accessLog")
 
   def rawTweets: JSONCollection = db.collection[JSONCollection]("rawTweets")
   def insertRawTweet(json: JsValue) = rawTweets.insert[JsValue](json)
-  
+
+  def accessLog: JSONCollection = db.collection[JSONCollection]("accessLog")
+
   val imagesGridFS = new GridFS(db, "images")
 }

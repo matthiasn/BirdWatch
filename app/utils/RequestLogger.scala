71ecfb22c8f93bb3c31bfe93e83ad540f9fca201
Matthias Nehlsen <mn@nehlsen-edv.de>
2013-04-28 19:24:36 +0200

    organizing imports

--- app/utils/RequestLogger.scala
+++ app/utils/RequestLogger.scala
@@ -3,10 +3,9 @@ package birdwatchUtils
 import play.api.mvc.{Request, AnyContent}
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.libs.json.{JsValue, Json}
-import org.joda.time.DateTime
-
-//import play.modules.reactivemongo.PlayBsonImplicits.JsValueWriter
 import play.api.libs.ws.WS
+
+import org.joda.time.DateTime
 import org.joda.time.format.ISODateTimeFormat
 
 object RequestLogger {

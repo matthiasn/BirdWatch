5baf8e58a58164c6f8f4b78a1403e2ea34f04e2c
Matthias Nehlsen <mn@nehlsen-edv.de>
2013-08-12 21:49:48 +0300

    version; cleanup

--- project/Build.scala
+++ project/Build.scala
@@ -5,12 +5,11 @@ import play.Project._
 object ApplicationBuild extends Build {
 
   val appName         = "BirdWatch"
-  val appVersion      = "1.0-SNAPSHOT"
+  val appVersion      = "0.2-SNAPSHOT"
 
   val appDependencies = Seq(
     "org.webjars" %% "webjars-play" % "2.1.0-2",
     "org.webjars" % "bootstrap" % "3.0.0-rc1",
-    "org.webjars" % "angular-ui-bootstrap" % "0.4.0",
     "org.webjars" % "angularjs" % "1.1.5-1",
     "org.webjars" % "jquery" % "1.9.1",
     "org.webjars" % "d3js" % "3.1.5",

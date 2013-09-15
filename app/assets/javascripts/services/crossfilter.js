ff861aa0df86c0c0ea2a078a0c3af50a6bc877b1
Matthias Nehlsen <mn@nehlsen-edv.de>
2013-09-15 19:50:34 +0200

    find retweets for original tweet in data set

--- app/assets/javascripts/services/crossfilter.js
+++ app/assets/javascripts/services/crossfilter.js
@@ -1,4 +1,4 @@
-//'use strict';
+'use strict';
 
 // crossfilter service
 angular.module('birdwatch.services').service('cf', function (utils) {
@@ -13,6 +13,10 @@ angular.module('birdwatch.services').service('cf', function (utils) {
         if (t.hasOwnProperty("retweeted_status")) { return t.retweeted_status.retweet_count; }
         else return 0;
     });
+    var originalIdDim  = cf.dimension(function(t) {
+        if (t.hasOwnProperty("retweeted_status")) { return t.retweeted_status.id; }
+        else return 0;
+    });
 
     // freeze imposes filter on crossfilter that only shows anything older than and including the latest
     // tweet at the time of calling freeze. Accordingly unfreeze clears the filter
@@ -24,12 +28,19 @@ angular.module('birdwatch.services').service('cf', function (utils) {
     exports.noItems   = function()         { return cf.size(); };                        // crossfilter size total
     exports.numPages  = function(pageSize) { return Math.ceil(cf.size() / pageSize); };  // number of pages
 
+    // predicates
+    var retweeted     = function(t) { return t.hasOwnProperty("retweeted_status"); };
+
     // mapper functions
     var originalTweet = function(t) { return utils.formatTweet(t.retweeted_status); };   // returns original tweet
     var tweetId       = function(t) { return t.id; };                                    // returns tweet id
-
-    // predicates
-    var retweeted     = function(t) { return t.hasOwnProperty("retweeted_status"); };
+    var retweetCount  = function(t) { if (retweeted(t)) { return t.retweeted_status.retweet_count; } else return 0 };
+    var maxRetweets   = function(t) {
+        t.retweet_count = retweetCount(_.max(originalIdDim.filter(t.id).top(1000),
+            function(t){ return t.retweeted_status.retweet_count; }));
+        originalIdDim.filterAll();
+        return t;
+    };
 
     // deliver tweets for current page. fetches all tweets up to the current page,
     // throws tweets for previous pages away.
@@ -41,7 +52,9 @@ angular.module('birdwatch.services').service('cf', function (utils) {
     // potentially mapped and filtered
     var fetchTweets = function(pageSize, order) {
       if      (order === "latest")    { return tweetIdDim.top(pageSize); }    // latest: desc order of tweets by ID
-      else if (order === "followers") { return followersDim.top(pageSize);}   // desc order of tweets by followers
+      else if (order === "followers") {
+          return followersDim.top(pageSize).map(maxRetweets);
+      }   // desc order of tweets by followers
       else if (order === "retweets") {  // descending order of tweets by total retweets of original message
           return _.first(               // filtered to be unique, would appear for each retweet in window otherwise
               _.uniq(retweetsDim.top(cf.size()).filter(retweeted).map(originalTweet), false, tweetId), pageSize);

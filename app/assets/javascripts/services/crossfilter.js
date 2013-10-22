77f54e1efd0bd2f545ab719cfe1677ac7065ca17
Matthias Nehlsen <mn@nehlsen-edv.de>
2013-10-23 01:44:51 +0200

    code formatting

--- app/assets/javascripts/services/crossfilter.js
+++ app/assets/javascripts/services/crossfilter.js
@@ -22,18 +22,18 @@ angular.module('birdwatch.services').factory('cf', function (utils) {
     // Example: returned function makes Jan 1, 2012, 16:45:00 out of Jan 1, 2012, 16:45:55 when interval is 60s
     function dateRound(s) { return function(t) { return s * Math.floor(Date.parse(t.created_at) / (s * 1000)) }; }
 
-    var byMinGrp   = cf.dimension(dateRound(      60)).group();
-    var by15MinGrp = cf.dimension(dateRound(   15*60)).group();
-    var byHourGrp  = cf.dimension(dateRound(   60*60)).group();
-    var by6HourGrp = cf.dimension(dateRound( 6*60*60)).group();
-    var byDayGrp   = cf.dimension(dateRound(24*60*60)).group();
+    var byMinGrp   = cf.dimension(dateRound(       60 )).group();
+    var by15MinGrp = cf.dimension(dateRound(    15*60 )).group();
+    var byHourGrp  = cf.dimension(dateRound(    60*60 )).group();
+    var by6HourGrp = cf.dimension(dateRound(  6*60*60 )).group();
+    var byDayGrp   = cf.dimension(dateRound( 24*60*60 )).group();
 
     exports.timeseries = function() {
-             if (byMinGrp.size() < 60)   { return byMinGrp.all(); }
+             if (byMinGrp.size()   < 60) { return byMinGrp.all();   }
         else if (by15MinGrp.size() < 48) { return by15MinGrp.all(); }
-        else if (byHourGrp.size() < 96)  { return byHourGrp.all(); }
+        else if (byHourGrp.size()  < 96) { return byHourGrp.all();  }
         else if (by6HourGrp.size() < 40) { return by6HourGrp.all(); }
-        else                             { return byDayGrp.all(); }
+        else                             { return byDayGrp.all();   }
     };
 
     // freeze imposes filter on crossfilter that only shows anything older than and including the latest
@@ -42,7 +42,7 @@ angular.module('birdwatch.services').factory('cf', function (utils) {
     exports.unfreeze  = function() { tweetIdDim.filterAll(); };
 
     exports.add       = function(data)     { cf.add(data); };                            // add new items, as array
-    exports.clear     = function()         { cf.remove(); };                             // reset crossfilter
+    exports.clear     = function()         { cf.remove();  };                            // reset crossfilter
     exports.noItems   = function()         { return cf.size(); };                        // crossfilter size total
     exports.numPages  = function(pageSize) { return Math.ceil(cf.size() / pageSize); };  // number of pages
 
@@ -69,9 +69,9 @@ angular.module('birdwatch.services').factory('cf', function (utils) {
     // fetch tweets from crossfilter dimension associated with particular sort order up to the current page,
     // potentially mapped and filtered
     var fetchTweets = function(pageSize, order) {
-      if      (order === "latest")    { return tweetIdDim.top(pageSize); }    // latest: desc order of tweets by ID
+           if (order === "latest")    { return tweetIdDim.top(pageSize); }    // latest: desc order of tweets by ID
       else if (order === "followers") { return followersDim.top(pageSize).map(maxRetweets); } // desc order of tweets by followers
-      else if (order === "retweets") {  // descending order of tweets by total retweets of original message
+      else if (order === "retweets")  { // descending order of tweets by total retweets of original message
           return _.first(               // filtered to be unique, would appear for each retweet in window otherwise
               _.uniq(retweetsDim.top(cf.size()).filter(retweeted).map(originalTweet), false, tweetId), pageSize);
       }

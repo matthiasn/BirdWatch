eb49d42bf674ac186150da82f0abee640557bac3
Matthias Nehlsen <mn@nehlsen-edv.de>
2014-03-30 03:55:28 +0200

    gruntfile plato bower file exclusion

--- react-js/gruntfile.js
+++ react-js/gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function (grunt) {
         plato: {
             bo: {
                 files: {
-                    'reports/': ['src/**/*.js']
+                    'reports/': ['src/*.js']
                 }
             }
         },

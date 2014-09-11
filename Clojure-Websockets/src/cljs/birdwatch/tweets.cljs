bab4e80fd1c909696b0ee03c6680f0c1099e312f
Matthias Nehlsen <mn@nehlsen-edv.de>
2014-09-11 13:33:09 +0200

    side-effecting functions marked with '!'

--- Clojure-Websockets/src/cljs/birdwatch/tweets.cljs
+++ Clojure-Websockets/src/cljs/birdwatch/tweets.cljs
@@ -9,7 +9,7 @@
 
 (enable-console-print!)
 
-(defn add-to-tweets-map [app tweets-map tweet]
+(defn add-to-tweets-map! [app tweets-map tweet]
   "adds tweet to tweets-map"
   (swap! app
          assoc-in [tweets-map (keyword (:id_str tweet))]
@@ -28,7 +28,7 @@
   "swaps item in priority-map when new value is larger than old value"
   (when (> n (rt-id (priority-map @app))) (util/swap-pmap app priority-map rt-id n)))
 
-(defn add-rt-status [app tweet]
+(defn add-rt-status! [app tweet]
   "handles original, retweeted tweet"
   (if (contains? tweet :retweeted_status)
     (let [state @app
@@ -39,29 +39,29 @@
       (swap-when-larger app :by-favorites rt-id (:favorite_count rt))
       (util/swap-pmap app :by-rt-since-startup rt-id (inc (get (:by-rt-since-startup state) rt-id 0)))
       (util/swap-pmap app :by-reach rt-id (+ (get (:by-reach state) rt-id 0) (:followers_count (:user tweet))))
-      (when (> rt-count (:retweet_count (rt-id (:tweets-map state)))) (add-to-tweets-map app :tweets-map rt)))))
+      (when (> rt-count (:retweet_count (rt-id (:tweets-map state)))) (add-to-tweets-map! app :tweets-map rt)))))
 
-(defn add-tweet [tweet app]
+(defn add-tweet! [tweet app]
   "increment counter, add tweet to tweets map and to sorted sets by id and by followers"
   (let [state @app
         id-str (:id_str tweet)
         id-key (keyword id-str)]
     (swap! app assoc :count (inc (:count state)))
-    (add-to-tweets-map app :tweets-map tweet)
+    (add-to-tweets-map! app :tweets-map tweet)
     (util/swap-pmap app :by-followers (keyword id-str) (:followers_count (:user tweet)))
     (util/swap-pmap app :by-id (keyword id-str) id-str)
     (util/swap-pmap app :by-reach id-key (+ (get (:by-reach state) id-key 0) (:followers_count (:user tweet))))
-    (add-rt-status app tweet)
+    (add-rt-status! app tweet)
     (wc/process-tweet app (:text tweet))))
 
 (go-loop []
  (let [[t chan] (alts! [c/tweets-chan c/prev-tweets-chan] {:priority true})]
-   (add-tweet t state/app)
+   (add-tweet! t state/app)
    (recur)))
 
 (go-loop []
  (let [mt (<! c/missing-tweet-found-chan)]
-   (add-to-tweets-map state/app :tweets-map mt)
+   (add-to-tweets-map! state/app :tweets-map mt)
    (recur)))
 
 (go-loop []

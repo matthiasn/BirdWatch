fe4344ac7ef03ffdbe2cba4369ff258afcbe6f5e
Matthias Nehlsen <mn@nehlsen-edv.de>
2014-11-04 00:56:26 +0100

    fixing issue with start / stop of Switchboard component
    Before this fix, restarting the entire systems on the REPL didn't
    work because apparently, the component library did not detect changes
    in the Switchboard component when no key/value pairs where assoc'd
    on the component map on startup, so it wasn't actually restarted.
    That, however, left the old channels wired instead of the new channels.

--- Clojure-Websockets/MainApp/src/clj/birdwatch/switchboard.clj
+++ Clojure-Websockets/MainApp/src/clj/birdwatch/switchboard.clj
@@ -6,28 +6,24 @@
    [clojure.core.async :as async :refer [chan mult tap pipe]]))
 
 ;;;; This component is the central switchboard for information flow in this application.
-;;;; The individual channel components come together like wiring harnesses in a car. One for the engine,
-;;;; one for the AC, one for the soundsystem and so on.
+;;;; The individual channel components come together like wiring harnesses in a car.
 
 (defrecord Switchboard [comm-chans pers-chans perc-chans iop-chans]
   component/Lifecycle
   (start [component] (log/info "Starting Switchboard Component")
          (let [tweets-mult (chan)]
-
            ;; Connect channels 1 on 1. Here, it would be easy to add message logging.
            (pipe (:tweet-count pers-chans) (:tweet-count comm-chans))
            (pipe (:register-perc comm-chans) (:register-percolation perc-chans))
-
-           ;(pipe (:percolation-matches perc-chans) (:perc-matches comm-chans))
-
            (pipe (:receive iop-chans) (:percolation perc-chans))
-
            (pipe (:percolation-matches perc-chans) (:perc-matches comm-chans))
-
            (pipe (:tweet-missing comm-chans) (:tweet-missing pers-chans))
            (pipe (:missing-tweet-found pers-chans) (:missing-tweet-found comm-chans))
            (pipe (:query comm-chans) (:query pers-chans))
-           (pipe (:query-results pers-chans) (:query-results comm-chans))))
-  (stop [component] (log/info "Stop Switchboard Component")))
+           (pipe (:query-results pers-chans) (:query-results comm-chans))
+           (assoc component :tweets-mult tweets-mult)))
+
+  (stop [component] (log/info "Stopping Switchboard Component")
+        (assoc component :tweets-mult nil)))
 
 (defn new-switchboard [] (map->Switchboard {}))
--- Clojure-Websockets/TwitterClient/src/clj/birdwatch_tc/switchboard.clj
+++ Clojure-Websockets/TwitterClient/src/clj/birdwatch_tc/switchboard.clj
@@ -6,8 +6,7 @@
    [clojure.core.async :as async :refer [chan mult tap pipe]]))
 
 ;;;; This component is the central switchboard for information flow in this application.
-;;;; The individual channel components come together like wiring harnesses in a car. One for the engine,
-;;;; one for the AC, one for the soundsystem and so on.
+;;;; The individual channel components come together like wiring harnesses in a car.
 
 (defrecord Switchboard [tc-chans pers-chans perc-chans iop-chans]
   component/Lifecycle
@@ -16,7 +15,10 @@
            (tap tweets-mult (:percolation perc-chans))   ; Tweets are distributed to multiple channels
            (tap tweets-mult (:persistence pers-chans))   ; through tapping the mult created above
            ;; Connect channels 1 on 1. Here, it would be easy to add message logging.
-           (pipe (:percolation-matches perc-chans) (:send iop-chans))))
-  (stop [component] (log/info "Stop Switchboard Component")))
+           (pipe (:percolation-matches perc-chans) (:send iop-chans))
+           (assoc component :tweets-mult tweets-mult)))
+
+  (stop [component] (log/info "Stop Switchboard Component")
+        (assoc component :tweets-mult nil)))
 
 (defn new-switchboard [] (map->Switchboard {}))

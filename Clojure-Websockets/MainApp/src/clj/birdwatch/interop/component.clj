a2d5e4fdfc8bbc369e46db3cfe2219e596f200f6
Matthias Nehlsen <mn@nehlsen-edv.de>
2014-11-04 01:27:28 +0100

    hold on to listener (so it can be closed on component shutdown)

--- Clojure-Websockets/MainApp/src/clj/birdwatch/interop/component.clj
+++ Clojure-Websockets/MainApp/src/clj/birdwatch/interop/component.clj
@@ -9,15 +9,17 @@
 
 ;;; The interop component allows sending and receiving messages via Redis Pub/Sub.
 ;;; It has both a :send and a :receive channel and can be used on both sides of the Pub/Sub.
-(defrecord Interop [conf channels conn]
+(defrecord Interop [conf channels listener]
   component/Lifecycle
   (start [component] (log/info "Starting Interop Component")
-         (let [conn {:pool {} :spec {:host (:redis-host conf) :port (:redis-port conf)}}]
-           (red/subscribe-topic (:receive channels) conn "matches")
-           ;(red/run-send-loop (:send channels) conn "matches")
-           (assoc component :conn conn)))
-  (stop  [component] (log/info "Stopping Interop Component") ;; TODO: proper teardown of resources
-         (assoc component :conn nil)))
+         (let [conn {:pool {} :spec {:host (:redis-host conf) :port (:redis-port conf)}}
+               listener (red/subscribe-topic (:receive channels) conn "matches")]
+           (assoc component :conn conn :listener listener)))
+
+  (stop  [component] (log/info "Stopping Interop Component")
+         (red/unsubscribe listener)
+         (red/close listener)
+         (assoc component :conn nil :listener nil)))
 
 (defn new-interop [conf] (map->Interop {:conf conf}))
 

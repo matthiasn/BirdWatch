1d0398830ea557d0f3d328fed5f1368491097197
Matthias Nehlsen <mn@nehlsen-edv.de>
2014-11-19 20:16:07 +0100

    inspecting data in communicator

--- Clojure-Websockets/MainApp/src/clj/birdwatch/communicator/websockets.clj
+++ Clojure-Websockets/MainApp/src/clj/birdwatch/communicator/websockets.clj
@@ -5,6 +5,7 @@
    [clojure.pprint :as pp]
    [clojure.tools.logging :as log]
    [taoensso.sente :as sente]
+   [com.matthiasnehlsen.inspect :as inspect :refer [inspect]]
    [clojure.core.async :as async :refer [<! >! put! timeout go-loop]]))
 
 (defn user-id-fn
@@ -33,15 +34,18 @@
 (defn tweet-stats
   "send stats about number of indexed tweets to all connected clients"
   [uids chsk-send!]
-  (fn [msg] (doseq [uid (:any @uids)] (chsk-send! uid [:stats/total-tweet-count msg]))))
+  (fn [msg] (doseq [uid (:any @uids)]
+              (chsk-send! uid [:stats/total-tweet-count msg]))))
 
 (defn perc-matches
   "deliver percolation matches to interested clients"
   [uids chsk-send!]
-  (fn [msg] (let [[t matches subscriptions] msg]
-              (doseq [uid (:any @uids)]
-                (when (contains? matches (get subscriptions uid))
-                  (chsk-send! uid [:tweet/new t]))))))
+  (fn [msg]
+    (inspect :comm/perc-matches msg)
+    (let [[t matches subscriptions] msg]
+      (doseq [uid (:any @uids)]
+        (when (contains? matches (get subscriptions uid))
+          (chsk-send! uid [:tweet/new t]))))))
 
 (defn relay-msg
   "send query result chunks back to client"
@@ -53,5 +57,6 @@
   [chsk-send! connected-uids]
   (go-loop [] (<! (timeout 2000))
            (let [uids (:any @connected-uids)]
+             (inspect :comm/connected-uids uids)
              (doseq [uid uids] (chsk-send! uid [:stats/users-count (count uids)])))
            (recur)))

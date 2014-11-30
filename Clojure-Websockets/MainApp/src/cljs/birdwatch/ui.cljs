0a10ba188c29cd70d7875d7f1147f484e678ab64
Matthias Nehlsen <mn@nehlsen-edv.de>
2014-11-30 21:27:30 +0100

    styling

--- Clojure-Websockets/MainApp/src/cljs/birdwatch/ui.cljs
+++ Clojure-Websockets/MainApp/src/cljs/birdwatch/ui.cljs
@@ -38,7 +38,7 @@
   "generates JS for sort button for both updating sort order and showing active button"
   [app key]
   #js {:onClick (fn [e] (om/update! app [:sorted] key))
-       :className (str "btn " (if (= key (:sorted app)) "btn-primary"))})
+       :className (str "pure-button" (if (= key (:sorted app)) " pure-button-primary" ""))})
 
 (defn sort-buttons-view
   "rendering sort buttons"
@@ -46,8 +46,8 @@
   (reify
     om/IRender
     (render [this]
-            (dom/div #js {:className "btn-group"}
-                     (dom/button #js {:className "btn"} "Sort by")
+            (dom/div nil
+                     (dom/button #js {:className "pure-button"} "Sort by")
                      (dom/button (sort-button-js app :by-id) "latest")
                      (dom/button (sort-button-js app :by-followers) "followers")
                      (dom/button (sort-button-js app :by-retweets) "retweets")
@@ -76,9 +76,8 @@
 (defn pag-items
   "function creating pagination items"
   [app page-change-chan]
-  (map #(dom/li nil
-                (dom/a #js {:className (if (= % (:page app)) "pure-button pure-button-active" "pure-button")
-                            :onClick (fn [e] (put! page-change-chan %))} %))
+  (map #(dom/button #js {:className (if (= % (:page app)) "pure-button pure-button-primary" "pure-button")
+                      :onClick (fn [e] (put! page-change-chan %))} %)
        (take 10 (range 1 (Math/floor (/ (:count app) (:n app)))))))
 
 (defn pagination-view
@@ -97,6 +96,6 @@
                           (recur))))))
     om/IRenderState
     (render-state [this {:keys [page-change]}]
-                  (apply dom/ul #js {:className "pure-paginator"}
-                         (flatten [(dom/li nil (dom/a #js {:className "pure-button"} "Page"))
+                  (apply dom/div nil
+                         (flatten [(dom/button #js {:className "pure-button"} "Page")
                                    (pag-items app page-change)])))))

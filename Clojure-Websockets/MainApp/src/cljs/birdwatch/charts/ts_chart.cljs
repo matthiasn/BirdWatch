ab3f7410945b3b6bc91fb67158d834dfec38aecc
Matthias Nehlsen <mn@nehlsen-edv.de>
2014-12-15 15:02:42 +0100

    timeseries chart separated into charts folder

---
+++ Clojure-Websockets/MainApp/src/cljs/birdwatch/charts/ts_chart.cljs
@@ -0,0 +1,47 @@
+(ns birdwatch.charts.ts-chart
+  (:require [birdwatch.util :as util]
+            [reagent.core :as r :refer [atom]]))
+
+(enable-console-print!)
+
+(def bars (atom []))
+(def label (atom {}))
+
+(def ts-elem (util/by-id "timeseries1"))
+(def ts-w (aget ts-elem "offsetWidth"))
+(def ts-h 100)
+
+(defn bar [x y h w idx]
+  [:rect {:x x :y (- y h) :fill "steelblue" :width w :height h
+          :on-mouse-enter #(reset! label {:idx idx})
+          :on-mouse-leave #(reset! label {})}])
+
+(defn barchart [indexed mx cnt w]
+    (let [gap (/ (/ ts-w 20) cnt)]
+      [:svg {:width ts-w :height ts-h}
+       [:g
+         (for [[idx [k v]] indexed]
+           [bar (* idx w) ts-h (* (/ v mx) ts-h) (- w gap) idx])]]))
+
+(defn labels [bars mx cnt w]
+  (when-not (empty? @label)
+    (let [idx (:idx @label)
+          [k v] (get bars idx)
+          top (- ts-h (* (/ v mx) ts-h))
+          lr (if (< (/ idx cnt) 0.6) "left" "right")]
+      [:div.detail {:style {:left (* idx w)}}
+       [:div.x_label {:class lr} (.toString (.unix js/moment k))]
+       [:div.item.active {:class lr :style {:top top}} "Tweets: " v]
+       [:div.dot.active {:style {:top top :border-color "steelblue"}}]])))
+
+(defn ts-chart []
+  (let [bars @bars
+        indexed (vec (map-indexed vector bars))
+        mx (apply max (map (fn [[k v]] v) bars))
+        cnt (count bars)
+        w (/ ts-w cnt)]
+    [:div.rickshaw_graph
+     [barchart indexed mx cnt w]
+     [labels bars mx cnt w]]))
+
+(r/render-component [ts-chart] ts-elem)
---
+++ Clojure-Websockets/MainApp/src/cljs/birdwatch/charts/wordcount_chart.cljs
@@ -0,0 +1,8 @@
+(ns birdwatch.charts.wordcount-chart
+  (:require [birdwatch.util :as util]
+            [birdwatch.stats.regression :as reg]
+            [reagent.core :as r :refer [atom]]))
+
+(enable-console-print!)
+
+

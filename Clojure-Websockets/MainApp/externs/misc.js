dc603e8db94bcb276beb2032f57089689c39c5c4
Matthias Nehlsen <mn@nehlsen-edv.de>
2014-12-16 01:24:30 +0100

    cleanup

--- Clojure-Websockets/MainApp/externs/misc.js
+++ Clojure-Websockets/MainApp/externs/misc.js
@@ -6,14 +6,3 @@ var BirdWatch = {};
 BirdWatch.WordCloud = function (w, h, maxEntries, addSearch, elem) {};
 BirdWatch.init = function () {};
 BirdWatch.redraw = function (dataSource) {};
-
-BirdWatch.updateBarchart = function (words) {};
-
-var Rickshaw = {};
-Rickshaw.Fixtures = {};
-Rickshaw.Fixtures.RandomData = {};
-Rickshaw.Fixtures.RandomData.addData = function (n) {};
-Rickshaw.Graph = {};
-Rickshaw.Graph.Axis = {};
-Rickshaw.Graph.Axis.Time = function (graph) {};
-Rickshaw.Graph.HoverDetail = function (graph) {};

8093419b2517d15875c31636f0f21cf83c00e52c
Matthias Nehlsen <mn@nehlsen-edv.de>
2015-01-12 02:11:48 +0100

    cleanup

--- Clojure-Websockets/MainApp/src/cljs/birdwatch/util.cljs
+++ Clojure-Websockets/MainApp/src/cljs/birdwatch/util.cljs
@@ -1,9 +1,6 @@
 (ns birdwatch.util
   (:require-macros [cljs.core.async.macros :refer [go-loop]])
-  (:require [clojure.string :as s]
-            [birdwatch.state :as state]
-            [tailrecursion.priority-map :refer [priority-map-by]]
-            [cljs.core.async :as async :refer [timeout]]))
+  (:require [cljs.core.async :as async :refer [timeout]]))
 
 (defn by-id [id] (.getElementById js/document id))
 (defn elem-width [elem] (aget elem "offsetWidth"))

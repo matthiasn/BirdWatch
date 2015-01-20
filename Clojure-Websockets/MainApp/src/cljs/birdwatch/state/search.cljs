8b9101e5d069ead9e995a01c49246e645d1b4d8f
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2015-01-20 15:07:58 +0100

    documentation

--- Clojure-Websockets/MainApp/src/cljs/birdwatch/state/search.cljs
+++ Clojure-Websockets/MainApp/src/cljs/birdwatch/state/search.cljs
@@ -2,11 +2,14 @@
   (:require [birdwatch.util :as util]
             [cljs.core.async :as async :refer [put!]]))
 
-(defn append-search-text [app s]
+(defn append-search-text
+  "Appends string s to search-text in app, separated by space."
+  [app s]
   (swap! app assoc :search-text (str (:search-text @app) " " s)))
 
 (defn- load-prev
-  "load previous tweets matching the current search"
+  "Loads previous tweets matching the current search. Search is contructed
+   by calling the util/query-string function with dereferenced app state."
   [app qry-chan]
   (let [chunks-to-load 10
         chunk-size 500
@@ -18,12 +21,13 @@
       (swap! app update-in [:prev-chunks-loaded] inc))))
 
 (defn- start-percolator
-  "trigger starting of percolation matching of new tweets"
+  "Triggers percolation matching of new tweets on the server side so that
+   future matches will be delivered to the client."
   [app qry-chan]
   (put! qry-chan [:cmd/percolate {:query (util/query-string @app)}]))
 
 (defn start-search
-  "Initiate new search"
+  "Initiates a new search."
   [app initial-state qry-chan]
   (let [search (:search-text @app)
         s (if (= search "") "*" search)]

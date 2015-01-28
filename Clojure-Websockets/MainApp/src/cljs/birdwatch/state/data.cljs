2cfa1c68d911418e57fad7a6fa363a868b24b65a
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2015-01-28 09:31:08 +0100

    proofreading

--- Clojure-Websockets/MainApp/src/cljs/birdwatch/state/data.cljs
+++ Clojure-Websockets/MainApp/src/cljs/birdwatch/state/data.cljs
@@ -6,7 +6,7 @@
   "Init app state and wire all channels required in the state namespace. The app
    atom is held inside the let binding of this function and thus protected from
    outside access / alteration. The only way to interact with it is by sending
-   messages on channels, such the provided data channel for adding new data or
+   messages on channels, such as the provided data channel for adding new data or
    sending commands on the cmd-chan."
   [data-chan qry-chan stats-chan cmd-chan state-pub-chan]
   (let [app (atom {})]

ec8b547d0e115bd5952c84c0dba5c6ac1736d05c
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2015-01-30 16:26:59 +0100

    comments; naming

--- Clojure-Websockets/MainApp/src/cljs/birdwatch/charts/ts_chart.cljs
+++ Clojure-Websockets/MainApp/src/cljs/birdwatch/charts/ts_chart.cljs
@@ -5,28 +5,34 @@
             [reagent.core :as r :refer [atom]]
             [cljs.core.async :as async :refer [chan sub timeout sliding-buffer]]))
 
-(enable-console-print!)
-
-(def bars (atom []))
+(def bars-atom (atom []))
 (def label (atom {}))
 
 (def ts-elem (util/by-id "timeseries1"))
 (def ts-w (aget ts-elem "offsetWidth"))
 (def ts-h 100)
 
-(defn bar [x y h w idx]
+(defn bar
+  "Renders a vertical bar. Enables showing a label when the mouse is
+   positioned above the bar."
+  [x y h w idx]
   [:rect {:x x :y (- y h) :fill "steelblue" :width w :height h
           :on-mouse-enter #(reset! label {:idx idx})
           :on-mouse-leave #(reset! label {})}])
 
-(defn barchart [indexed mx cnt w]
+(defn barchart
+  "Renders a bar chart, making use of the bar function above. Returns
+   entire SVG element."
+  [indexed mx cnt w]
   (let [gap (/ (/ ts-w 20) cnt)]
     [:svg {:width ts-w :height ts-h}
      [:g
       (for [[idx [k v]] indexed]
         ^{:key k} [bar (* idx w) ts-h (* (/ v mx) ts-h) (- w gap) idx])]]))
 
-(defn labels [bars mx cnt w]
+(defn labels
+  "Renders a label for a bar chart. Makes use of Rickshaw's CSS."
+  [bars mx cnt w]
   (when-not (empty? @label)
     (let [idx (:idx @label)
           [k v] (get bars idx)
@@ -37,8 +43,12 @@
        [:div.item.active {:class lr :style {:top top}} "Tweets: " v]
        [:div.dot.active {:style {:top top :border-color "steelblue"}}]])))
 
-(defn ts-chart []
-  (let [bars @bars
+(defn ts-chart
+  "Renders time series chart consisting of SVG for the bars and a label.
+   Appearance is similar to the Rickshaw timeseries chart, which this
+   component replaced, except for the CSS."
+  []
+  (let [bars @bars-atom
         indexed (vec (map-indexed vector bars))
         mx (apply max (map (fn [[k v]] v) bars))
         cnt (count bars)
@@ -55,7 +65,7 @@
   (let [state-chan (chan (sliding-buffer 1))]
     (go-loop []
              (let [[_ state] (<! state-chan)]
-               (reset! bars (ts/ts-data state))
+               (reset! bars-atom (ts/ts-data state))
                (<! (timeout every-ms))
                (recur)))
     (sub state-pub :app-state state-chan)))

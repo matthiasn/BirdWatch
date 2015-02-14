aaf99ad3604d4c9fb6285a7c270ba584387a1ce4
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2015-02-14 23:34:21 +0100

    pipe-with-metric now takes vector with i and out channels

--- Clojure-Websockets/MainApp/src/clj/birdwatch/metrics/tools.clj
+++ Clojure-Websockets/MainApp/src/clj/birdwatch/metrics/tools.clj
@@ -5,11 +5,12 @@
 
 (defn pipe-w-metric
   "Pipe messages from one channel to another while also sending the msg to the Metrics
-   component where e.g. meters can be derived from the data. Puts msg on metrics-chan.
-   Also takes the type of the message as a namespaced keyword."
+   component where e.g. meters can be derived from the data. Channels are passes as a vector:
+   [in-chan out-chan]. Puts msg on metrics-chan.
+  Also takes the type of the message as a namespaced keyword."
   [metrics-chan]
   (fn
-    [{in :in out :out} ev-type]
+    [[in out] ev-type]
     (let [m (mult in)
           t-chan (chan 1 (map (fn [ev] [ev-type ev])))]
       (tap m out)
--- Clojure-Websockets/MainApp/src/clj/birdwatch/switchboard.clj
+++ Clojure-Websockets/MainApp/src/clj/birdwatch/switchboard.clj
@@ -14,30 +14,22 @@
   (start [component] (log/info "Starting Switchboard Component")
          (let [tweets-mult (chan)
                pipe-w-metric (mt/pipe-w-metric (:events metrics-chans))
-               perc          {:in (:receive iop-chans)
-                              :out (:percolation perc-chans)}
-               register-perc {:in (:register-perc comm-chans)
-                              :out (:register-percolation perc-chans)}
-               tweet-count   {:in (:tweet-count pers-chans)
-                              :out (:tweet-count comm-chans)}
-               perc-matches  {:in (:percolation-matches perc-chans)
-                              :out (:perc-matches comm-chans)}
-               tweet-missing {:in (:tweet-missing comm-chans)
-                              :out (:tweet-missing pers-chans)}
-               missing-found {:in (:missing-tweet-found pers-chans)
-                              :out (:missing-tweet-found comm-chans)}
-               query         {:in (:query comm-chans)
-                              :out (:query pers-chans)}
-               query-result  {:in (:query-results pers-chans)
-                              :out (:query-results comm-chans)}]
-           (pipe-w-metric tweet-count :stats/tweet-count)
-           (pipe-w-metric register-perc :stats/register-perc)
-           (pipe-w-metric perc :stats/redis-received)
-           (pipe-w-metric perc-matches :stats/perc-matches)
-           (pipe-w-metric tweet-missing :stats/missing-t)
-           (pipe-w-metric missing-found :stats/tm-found)
-           (pipe-w-metric query :stats/query)
-           (pipe-w-metric query-result :stats/query-res)
+               perc          [(:receive iop-chans)             (:percolation perc-chans)]
+               register-perc [(:register-perc comm-chans)      (:register-percolation perc-chans)]
+               tweet-count   [(:tweet-count pers-chans)        (:tweet-count comm-chans)]
+               perc-matches  [(:percolation-matches perc-chans)(:perc-matches comm-chans)]
+               tweet-missing [(:tweet-missing comm-chans)      (:tweet-missing pers-chans)]
+               missing-found [(:missing-tweet-found pers-chans)(:missing-tweet-found comm-chans)]
+               query         [(:query comm-chans)              (:query pers-chans)]
+               query-result  [(:query-results pers-chans)      (:query-results comm-chans)]]
+           (pipe-w-metric tweet-count :meters/sb-tweet-count)
+           (pipe-w-metric register-perc :meters/sb-register-perc)
+           (pipe-w-metric perc :meters/sb-redis-received)
+           (pipe-w-metric perc-matches :meters/sb-perc-matches)
+           (pipe-w-metric tweet-missing :meters/sb-missing-t)
+           (pipe-w-metric missing-found :meters/sb-tm-found)
+           (pipe-w-metric query :meters/sb-query)
+           (pipe-w-metric query-result :meters/sb-query-res)
            (assoc component :tweets-mult tweets-mult)))
 
   (stop [component] (log/info "Stopping Switchboard Component")

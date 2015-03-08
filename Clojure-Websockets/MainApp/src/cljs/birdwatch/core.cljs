d0f5734a7c658b6d96c52d0745df0a8efd644da7
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2015-03-08 18:47:43 +0100

    instantiation of state component w/ multiple out channels

--- Clojure-Websockets/MainApp/src/cljs/birdwatch/core.cljs
+++ Clojure-Websockets/MainApp/src/cljs/birdwatch/core.cljs
@@ -13,14 +13,14 @@
             [com.matthiasnehlsen.systems-toolbox.core :as toolbox]
             [com.matthiasnehlsen.systems-toolbox.reagent :as toolbox-r]
             [com.matthiasnehlsen.systems-toolbox.sente :as toolbox-ws]
-            [cljs.core.async :refer [chan pub sub buffer sliding-buffer pipe]]))
+            [cljs.core.async :refer [chan pub sub <! >! buffer sliding-buffer pipe]]))
 
 (enable-console-print!)
 
 ;;;; Main file of the BirdWatch client-side application.
 
 ;;; Channels for handling information flow in the application.
-(def state-in-chan  (chan)) ; Data from server, e.g. new tweets and previous chunks.
+(def state-in-chan (chan)) ; Data from server, e.g. new tweets and previous chunks.
 (def qry-chan   (chan)) ; Queries that will be forwarded to the server.
 (def state-pub-chan (chan (sliding-buffer 1))) ; Publication of state changes.
 (def state-pub (pub state-pub-chan first)) ; Pub for subscribing to
@@ -30,16 +30,15 @@
 (cloud/mount-wordcloud state-pub state-in-chan {:n 250 :every-ms 5000})
 (ts-c/mount-ts-chart   state-pub {:every-ms 1000})
 
-(def state-comp (toolbox/component-with-channels state/init-component (buffer 10000) (buffer 10000)))
+(def state-comp (toolbox/component-with-channels-2 
+                  state/init-component
+                  {:in-chan [:buffer 1]
+                   :out-chans {:state-pub-out [:sliding 1] :qry-out [:buffer 1]}}
+                  #(match % [:app-state _] :state-pub-out :else :qry-out)))
+
 (pipe state-in-chan (:in-chan state-comp))
-(go-loop []
-         (let [msg (<! (:out-chan state-comp))]
-           (match msg
-                  [:cmd/query _] (>! qry-chan msg)
-                  [:cmd/percolate _] (>! qry-chan msg)
-                  [:app-state _] (>! state-pub-chan msg)
-                  :else (prn "else" msg))
-           (recur)))
+(pipe (-> state-comp :out-chans :qry-out) qry-chan)
+(pipe (-> state-comp :out-chans :state-pub-out) state-pub-chan)
 
 ;;; Initialization of WebSocket communication.
 (def ws-comp (toolbox-ws/component (buffer 1000) (buffer 1000)))

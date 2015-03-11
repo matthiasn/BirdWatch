579f960fb79b94d3effdfd98b8bc1489f3357777
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2015-03-11 06:47:36 +0100

    new reagent components

--- Clojure-Websockets/MainApp/src/cljs/birdwatch/core.cljs
+++ Clojure-Websockets/MainApp/src/cljs/birdwatch/core.cljs
@@ -23,15 +23,26 @@
 (def wc-comp (toolbox/make-component cloud/make-state nil cloud/state-pub-handler {:sliding-in-timeout 5000}))
 (def wc-c-comp (toolbox/make-component wc-c/make-state nil wc-c/state-pub-handler {:sliding-in-timeout 1000}))
 (def ts-comp (toolbox/make-component ts-c/make-state nil ts-c/state-pub-handler {:sliding-in-timeout 1000}))
+(def ws-comp (toolbox-ws/component))
+(def count-comp (toolbox-r/component cv/count-view "tweet-count"))
+(def users-count-comp (toolbox-r/component cv/users-count-view "users-count"))
+(def search-comp (toolbox-r/component sv/search-view "search"))
+(def sort-comp (toolbox-r/component st/sort-view "sort-buttons"))
+(def pag-comp (toolbox-r/component pag/pagination-view "pagination"))
+(def ttc-comp (toolbox-r/component cv/total-count-view "total-tweet-count"))
 
 (def tweets-comp (toolbox/single-in-single-out tw/init-component))
-
-;;; Initialization of WebSocket communication.
-(def ws-comp (toolbox-ws/component))
 
 (sub (:state-pub state-comp) :app-state (:sliding-in-chan wc-c-comp))
 (sub (:state-pub state-comp) :app-state (:sliding-in-chan ts-comp))
 (sub (:state-pub state-comp) :app-state (:sliding-in-chan wc-comp))
+(sub (:state-pub state-comp) :app-state (:sliding-in-chan count-comp))
+(sub (:state-pub state-comp) :app-state (:sliding-in-chan users-count-comp))
+(sub (:state-pub state-comp) :app-state (:sliding-in-chan search-comp))
+(sub (:state-pub state-comp) :app-state (:sliding-in-chan sort-comp))
+(sub (:state-pub state-comp) :app-state (:sliding-in-chan pag-comp))
+(sub (:state-pub state-comp) :app-state (:sliding-in-chan ttc-comp))
+
 (sub (:state-pub state-comp) :app-state (:in-chan tweets-comp))
 
 (pipe (:out-chan wc-c-comp)   (:in-chan state-comp))
@@ -39,10 +50,3 @@
 (pipe (:out-chan state-comp)  (:in-chan ws-comp))
 (pipe (:out-chan ws-comp)     (:in-chan state-comp))
 (pipe (:out-chan tweets-comp) (:in-chan state-comp))
-
-(toolbox-r/init-components (:state-pub state-comp) (:in-chan state-comp)
-                           [[cv/count-view "tweet-count"] [cv/users-count-view "users-count"]
-                            [cv/total-count-view "total-tweet-count"] [sv/search-view "search"]
-                            [pag/pagination-view "pagination"] [st/sort-view "sort-buttons"]])
-
-

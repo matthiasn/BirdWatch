a56731d9ebc805bc8611d014e659519e453e9d60
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2015-03-11 05:05:37 +0100

    change ts-chart to state in single atom

--- Clojure-Websockets/MainApp/src/cljs/birdwatch/charts/ts_chart.cljs
+++ Clojure-Websockets/MainApp/src/cljs/birdwatch/charts/ts_chart.cljs
@@ -10,26 +10,26 @@
 (defn bar
   "Renders a vertical bar. Enables showing a label when the mouse is
    positioned above the bar."
-  [x y h w idx label]
+  [x y h w idx app]
   [:rect {:x x :y (- y h) :fill "steelblue" :width w :height h
-          :on-mouse-enter #(reset! label {:idx idx})
-          :on-mouse-leave #(reset! label {})}])
+          :on-mouse-enter #(swap! app {:idx idx})
+          :on-mouse-leave #(swap! app assoc :label {})}])
 
 (defn barchart
   "Renders a bar chart, making use of the bar function above. Returns
    entire SVG element."
-  [indexed mx cnt w label]
+  [indexed mx cnt w app]
   (let [gap (/ (/ ts-w 20) cnt)]
     [:svg {:width ts-w :height ts-h}
      [:g
       (for [[idx [k v]] indexed]
-        ^{:key k} [bar (* idx w) ts-h (* (/ v mx) ts-h) (- w gap) idx label])]]))
+        ^{:key k} [bar (* idx w) ts-h (* (/ v mx) ts-h) (- w gap) idx app])]]))
 
 (defn labels
   "Renders a label for a bar chart. Makes use of Rickshaws CSS."
   [bars mx cnt w label]
-  (when-not (empty? @label)
-    (let [idx (:idx @label)
+  (when-not (empty? label)
+    (let [idx (:idx label)
           [k v] (get (vec bars) idx)
           top (- ts-h (* (/ v mx) ts-h))
           lr (if (< (/ idx cnt) 0.6) "left" "right")]
@@ -42,21 +42,20 @@
   "Renders time series chart consisting of SVG for the bars and a label.
    Appearance is similar to the Rickshaw timeseries chart, which this
    component replaced, except for the CSS."
-  [bars-atom label]
-  (let [bars @bars-atom
+  [app]
+  (let [bars (:bars @app)
         indexed (vec (map-indexed vector bars))
         mx (apply max (map (fn [[_ v]] v) bars))
         cnt (count bars)
         w (/ ts-w cnt)]
     [:div.rickshaw_graph
-     [barchart indexed mx cnt w label]
-     [labels bars mx cnt w label]]))
+     [barchart indexed mx cnt w app]
+     [labels bars mx cnt w (:label @app)]]))
 
 (defn init-component
   "Initializes timeseries chart component. Takes put-fn as the function that can be called when some message
    needs to be sent back to the switchboard. Returns a function that handles incoming messages."
   [_]
-  (let [bars (atom [])
-        label (atom {})]
-    (r/render-component [ts-chart bars label] ts-elem)
-    (fn [[_ state]] (reset! bars (ts/ts-data state)))))
+  (let [app (atom {:bars [] :label {}})]
+    (r/render-component [ts-chart app] ts-elem)
+    (fn [[_ state]] (swap! app assoc :bars (ts/ts-data state)))))

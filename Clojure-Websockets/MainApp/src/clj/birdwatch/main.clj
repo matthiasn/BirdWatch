27803346250a7201e986d04a89f158048222b359
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2015-03-26 12:37:01 +0100

    .

--- Clojure-Websockets/MainApp/src/clj/birdwatch/main.clj
+++ Clojure-Websockets/MainApp/src/clj/birdwatch/main.clj
@@ -10,7 +10,8 @@
    [clojure.tools.namespace.repl :refer (refresh)]
    [clj-pid.core :as pid]
    [matthiasn.systems-toolbox.switchboard :as sb]
-   [matthiasn.systems-toolbox.sente :as sente]))
+   [matthiasn.systems-toolbox.sente :as sente]
+   [matthiasn.systems-toolbox.scheduler :as sched]))
 
 (def conf (edn/read-string (slurp "conf.edn")))
 
@@ -20,15 +21,18 @@
     (sb/send-mult-cmd
       switchboard
       [[:cmd/wire-comp (sente/component markup/index 8888)]
-       [:cmd/tap-comp [:ws-cmp :log-cmp]]
+       [:cmd/wire-comp (sched/component)]
        [:cmd/wire-comp (pc/component :persistence-cmp conf)]
+       [:cmd/wire-comp (iop-cmp/component :interop-cmp conf)]
+       [:cmd/wire-comp (perc-cmp/component :percolator-cmp conf)]
+       [:cmd/tap-comp [:ws-cmp :log-cmp]]
+       [:cmd/sub-comp [:persistence-cmp :cmd/schedule-new :scheduler-cmp]]
+       [:cmd/sub-comp [:scheduler-cmp :schedule/count :persistence-cmp]]
        [:cmd/sub-comp [:ws-cmp :cmd/query :persistence-cmp]]
        [:cmd/sub-comp [:ws-cmp :cmd/missing :persistence-cmp]]
        [:cmd/sub-comp [:persistence-cmp :tweet/prev-chunk :ws-cmp]]
        [:cmd/sub-comp [:persistence-cmp :tweet/missing-tweet :ws-cmp]]
        [:cmd/sub-comp [:persistence-cmp :log/info :log-cmp]]
-       [:cmd/wire-comp (iop-cmp/component :interop-cmp conf)]
-       [:cmd/wire-comp (perc-cmp/component :percolator-cmp conf)]
        [:cmd/sub-comp [:interop-cmp :redis/matches :percolator-cmp]]
        [:cmd/sub-comp [:ws-cmp :cmd/percolate :percolator-cmp]]
        [:cmd/sub-comp [:percolator-cmp :tweet/new :ws-cmp]]

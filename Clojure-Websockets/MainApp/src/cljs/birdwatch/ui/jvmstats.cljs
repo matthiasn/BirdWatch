2afd8f03ab1d33241000ee4f3ba3eb6a5cf92f6b
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2015-04-01 17:54:31 +0200

    Preliminary vertical bar chart for CPU load
    Needs some heavy refactoring to make charts generic. Chart function
    should be usable for memory usage or whatever.

--- Clojure-Websockets/MainApp/src/cljs/birdwatch/ui/jvmstats.cljs
+++ Clojure-Websockets/MainApp/src/cljs/birdwatch/ui/jvmstats.cljs
@@ -7,20 +7,36 @@
             [goog.string :as gstring]
             [goog.string.format]))
 
+(defn bar
+  "Renders a vertical bar."
+  [x y h w]
+  [:rect {:x x :y (- y h) :fill "steelblue" :width w :height h}])
+
 (defn reading-view
   ""
   [app]
   (let [state @app
         fmt #(gstring/format %1 %2)
         floor #(Math/floor %)
-        latest (last (:readings state))
+        readings (:readings state)
+        cpu-loads (map :system-load-avg readings)
+        last-n (take-last 60 cpu-loads)
+        mx 8 ; workaround for some problem with actual mx function, only accurate on quadcore machine with HT
+        indexed (vec (map-indexed vector last-n))
+        latest (last readings)
         sys-load-avg (:system-load-avg latest)
         available-cpus (:available-cpus latest)
         uptime (:uptime latest)
-        m 60000
-        s 1000]
+        m 60000 s 1000          ; minutes and seconds in milliseconds for uptime calculation
+        w 4 gap 1               ; bar width and gap between bars
+        chart-w 400 chart-h 40] ; chart dimensions
     [:div
      [:div [:strong "System Load Average: "] (fmt "%.2f" (-> sys-load-avg (/ available-cpus) (* 100))) "%"]
+     [:svg {:width chart-w :height chart-h}
+          [:g
+           (for [[idx v] indexed]
+             ^{:key (str idx v)} [bar (* idx w) chart-h (* (/ v mx) chart-h) (- w gap)])]]
+     [:hr]
      [:div [:strong "CPUs: "] available-cpus]
      [:div [:strong "Uptime: "] (floor (/ uptime m)) "m" (floor (/ (rem uptime m) s)) "s"]]))
 

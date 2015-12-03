6590faabb46105f31154c21412905af8a2038bef
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2015-12-03 20:39:18 +0100

    updated depencies, conflicts resolved

--- Clojure-Websockets/TwitterClient/project.clj
+++ Clojure-Websockets/TwitterClient/project.clj
@@ -4,14 +4,15 @@
   :url "https://github.com/matthiasn/Birdwatch"
   :license {:name "GNU General Public License" :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.8.0-RC3"]
-                 [twitter-api "0.7.8"]
+                 [clj-http "2.0.0"]
+                 [twitter-api "0.7.8" :exclusions [org.clojure/data.json]]
                  [clojurewerkz/elastisch "2.2.0-beta4"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [matthiasn/systems-toolbox "0.3.14"]
+                 [matthiasn/systems-toolbox "0.3.15"]
                  [ch.qos.logback/logback-classic "1.1.3"]
                  [clj-time "0.11.0"]
                  [clj-pid "0.1.2"]
-                 [com.taoensso/carmine "2.12.1"]
+                 [com.taoensso/carmine "2.12.1" :exclusions [org.clojure/tools.reader]]
                  [metrics-clojure "2.6.0"]]
   :source-paths ["src/clj/"]
   :main ^:skip-aot birdwatch-tc.main

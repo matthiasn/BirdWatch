03ed61eb80c088c6dd8fe17ff718ca4c4245eb90
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2015-12-03 20:18:46 +0100

    Clojure 1.8.0-RC3, ClojureScript 1.7.189, systems-toolbox v0.3.15
    Also: confusing dependencies resolved

--- Clojure-Websockets/MainApp/project.clj
+++ Clojure-Websockets/MainApp/project.clj
@@ -3,14 +3,15 @@
   :description "Main part of the BirdWatch system (without TwitterClient)"
   :url "https://github.com/matthiasn/Birdwatch"
   :license {:name "GNU General Public License" :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.170"]
+  :dependencies [[org.clojure/clojure "1.8.0-RC3"]
+                 [org.clojure/tools.reader "1.0.0-alpha1"]
+                 [org.clojure/clojurescript "1.7.189"]
                  [clojurewerkz/elastisch "2.2.0-beta4"]
                  [org.clojure/tools.logging "0.3.1"]
                  [com.taoensso/timbre "4.1.4"]
                  [com.taoensso/encore "2.26.1"]
                  [com.taoensso/carmine "2.12.1"]
-                 [matthiasn/systems-toolbox "0.3.14"]
+                 [matthiasn/systems-toolbox "0.3.15"]
                  [org.clojure/tools.namespace "0.2.11"]
                  [ch.qos.logback/logback-classic "1.1.3"]
                  [hiccup "1.0.5"]
@@ -33,9 +34,15 @@
   :profiles {:uberjar {:aot :all}}
 
   :plugins [[lein-cljsbuild "1.1.1"]
-            [lein-figwheel "0.5.0-2" :exclusions [org.clojure/clojure org.codehaus.plexus/plexus-utils]]
+            [lein-figwheel "0.5.0-2" :exclusions [org.clojure/clojure
+                                                  org.clojure/clojurescript
+                                                  org.clojure/tools.reader
+                                                  org.clojure/google-closure-library
+                                                  commons-fileupload
+                                                  ring/ring-core
+                                                  org.codehaus.plexus/plexus-utils]]
             [quickie "0.4.1" :exclusions [org.clojure/clojure org.codehaus.plexus/plexus-utils]]
-            [codox "0.9.0"]]
+            [codox "0.9.0" :exclusions [org.clojure/tools.reader org.clojure/tools.namespace]]]
 
   :figwheel {:server-port 3452
              :css-dirs    ["resources/public/css"]}
--- Clojure-Websockets/TwitterClient/project.clj
+++ Clojure-Websockets/TwitterClient/project.clj
@@ -3,7 +3,7 @@
   :description "Twitter client part of the BirdWatch system"
   :url "https://github.com/matthiasn/Birdwatch"
   :license {:name "GNU General Public License" :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0-RC3"]
                  [twitter-api "0.7.8"]
                  [clojurewerkz/elastisch "2.2.0-beta4"]
                  [org.clojure/tools.logging "0.3.1"]

c5b28358ed84185c37d5d8594827bc9a7b31363a
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2015-12-26 02:54:23 +0100

    latest dependency versions

--- Clojure-Websockets/TwitterClient/project.clj
+++ Clojure-Websockets/TwitterClient/project.clj
@@ -3,12 +3,12 @@
   :description "Twitter client part of the BirdWatch system"
   :url "https://github.com/matthiasn/Birdwatch"
   :license {:name "GNU General Public License" :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
-  :dependencies [[org.clojure/clojure "1.8.0-RC3"]
+  :dependencies [[org.clojure/clojure "1.8.0-RC4"]
                  [clj-http "2.0.0"]
                  [twitter-api "0.7.8" :exclusions [org.clojure/data.json]]
-                 [clojurewerkz/elastisch "2.2.0-beta4"]
+                 [clojurewerkz/elastisch "2.2.0-rc1"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [matthiasn/systems-toolbox "0.4.3"]
+                 [matthiasn/systems-toolbox "0.4.8"]
                  [ch.qos.logback/logback-classic "1.1.3"]
                  [clj-time "0.11.0"]
                  [clj-pid "0.1.2"]

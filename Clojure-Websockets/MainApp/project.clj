81583362ff83aa6c2d497c718673c3df37204393
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2016-07-25 17:39:47 +0200

    dependencies

--- Clojure-Websockets/MainApp/project.clj
+++ Clojure-Websockets/MainApp/project.clj
@@ -2,19 +2,19 @@
   :description "Main part of the BirdWatch system (without TwitterClient)"
   :url "https://github.com/matthiasn/Birdwatch"
   :license {:name "GNU General Public License" :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha8"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha10"]
                  [org.clojure/clojurescript "1.9.93" :exclusions [org.clojure/tools.reader]]
-                 [clojurewerkz/elastisch "2.2.1" :exclusions [commons-io potemkin]]
+                 [clojurewerkz/elastisch "2.2.2" :exclusions [commons-io potemkin]]
                  [com.rpl/specter "0.11.2"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [com.taoensso/timbre "4.5.1" :exclusions [io.aviso/pretty]]
-                 [com.taoensso/encore "2.62.0" :exclusions [org.clojure/tools.reader]]
-                 [com.taoensso/carmine "2.13.1" :exclusions [org.clojure/tools.reader]]
-                 [matthiasn/systems-toolbox "0.6.1-SNAPSHOT"]
-                 [matthiasn/systems-toolbox-sente "0.6.1-SNAPSHOT"]
-                 [matthiasn/systems-toolbox-ui "0.6.1-SNAPSHOT"]
-                 [matthiasn/systems-toolbox-metrics "0.6.1-SNAPSHOT"]
-                 [matthiasn/systems-toolbox-redis "0.6.1-SNAPSHOT"]
+                 [com.taoensso/timbre "4.7.3"]
+                 [com.taoensso/encore "2.68.0"]
+                 [com.taoensso/carmine "2.14.0"]
+                 [matthiasn/systems-toolbox "0.6.1-alpha2"]
+                 [matthiasn/systems-toolbox-sente "0.6.1-alpha4"]
+                 [matthiasn/systems-toolbox-ui "0.6.1-alpha6"]
+                 [matthiasn/systems-toolbox-metrics "0.6.1-alpha1"]
+                 [matthiasn/systems-toolbox-redis "0.6.1-alpha1"]
                  [matthiasn/birdwatch-specs "0.3.1"]
                  [org.clojure/tools.namespace "0.2.11"]
                  [ch.qos.logback/logback-classic "1.1.7"]
@@ -43,7 +43,7 @@
              :http2   {:jvm-opts ["-Xbootclasspath/p:TLS/alpn-boot-8.1.7.v20160121.jar"]}}
 
   :plugins [[lein-cljsbuild "1.1.3"]
-            [lein-figwheel "0.5.4-5"]
+            [lein-figwheel "0.5.4-7"]
             [codox "0.9.5"]]
 
   :figwheel {:server-port 3452
--- Clojure-Websockets/TwitterClient/project.clj
+++ Clojure-Websockets/TwitterClient/project.clj
@@ -2,19 +2,19 @@
   :description "Twitter client part of the BirdWatch system"
   :url "https://github.com/matthiasn/Birdwatch"
   :license {:name "GNU General Public License" :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha8"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha10"]
                  [clj-http "3.1.0"]
                  [twitter-api "0.7.8" :exclusions [org.clojure/data.json]]
                  [org.clojure/core.match "0.3.0-alpha4"]
-                 [clojurewerkz/elastisch "2.2.1"]
+                 [clojurewerkz/elastisch "2.2.2"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [matthiasn/systems-toolbox "0.6.1-SNAPSHOT"]
-                 [matthiasn/systems-toolbox-redis "0.6.1-SNAPSHOT"]
+                 [matthiasn/systems-toolbox "0.6.1-alpha2"]
+                 [matthiasn/systems-toolbox-redis "0.6.1-alpha1"]
                  [matthiasn/birdwatch-specs "0.3.1"]
                  [ch.qos.logback/logback-classic "1.1.7"]
                  [clj-time "0.12.0"]
                  [clj-pid "0.1.2"]
-                 [com.taoensso/carmine "2.13.1"]]
+                 [com.taoensso/carmine "2.14.0"]]
   :source-paths ["src/clj/"]
   :main ^:skip-aot birdwatch-tc.main
   :target-path "target/%s"

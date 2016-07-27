9140892a5dce3032e2c3e1bce0517e4aa3f910cf
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2016-07-27 15:07:41 +0200

    config path; line lengths

--- Clojure-Websockets/TwitterClient/src/clj/birdwatch_tc/main.clj
+++ Clojure-Websockets/TwitterClient/src/clj/birdwatch_tc/main.clj
@@ -15,31 +15,35 @@
 (pretty/install-pretty-logging)
 (pretty/install-uncaught-exception-handler)
 
-(def conf (edn/read-string (slurp "twitterconf.edn")))
+(def conf-filepath (get (System/getenv) "CONF_PATH" "twitterconf.edn"))
+(def conf (edn/read-string (slurp conf-filepath)))
 
 (defn restart!
-  "Starts (or restarts) a system built out of the specified subsystems. The switchboard will
-  then fire up subsystems according to the blueprint maps, which are in passed in the second
-  position of the :cmd/init-comp vectors. These subsystems are then wired to provide the
-  communication paths required by the application.
-  The system can be restarted on the REPL. For example, say we modify the
-  birdwatch.percolator.percolator namespace. Then, we can reload it:
+  "Starts (or restarts) a system built out of the specified subsystems. The
+   switchboard will then fire up subsystems according to the blueprint maps,
+   which are in passed in the second position of the :cmd/init-comp vectors.
+   These subsystems are then wired to provide the communication paths required
+   by the application.
+   The system can be restarted on the REPL. For example, say we modify the
+   birdwatch.percolator.percolator namespace. Then, we can reload it:
 
       (require '[birdwatch-tc.percolator.percolator :as perc] :reload)
 
-  Then, calling this function again will restart the system while maintaining the state of
-  the individual subsystems."
+   Then, calling this function again will restart the system while maintaining
+   the state of the individual subsystems."
   []
   (let [switchboard (sb/component :tc/switchboard)]
     (sb/send-mult-cmd
       switchboard
-      [[:cmd/init-comp #{(tc/cmp-map :tc/client-cmp conf)
-                         (sched/cmp-map :tc/scheduler-cmp)
-                         (pc/cmp-map :tc/persistence-cmp conf)
-                         (redis/cmp-map :tc/interop-cmp (merge (:redis conf)
-                                                               {:relay-types #{:perc/matches}}))
-                         (perc/cmp-map :tc/percolator-cmp conf)}]
-       [:cmd/route {:from :tc/client-cmp :to #{:tc/persistence-cmp :tc/percolator-cmp}}]
+      [[:cmd/init-comp
+        #{(tc/cmp-map :tc/client-cmp conf)
+          (sched/cmp-map :tc/scheduler-cmp)
+          (pc/cmp-map :tc/persistence-cmp conf)
+          (redis/cmp-map :tc/interop-cmp
+                         (merge (:redis conf) {:relay-types #{:perc/matches}}))
+          (perc/cmp-map :tc/percolator-cmp conf)}]
+       [:cmd/route {:from :tc/client-cmp :to #{:tc/persistence-cmp
+                                               :tc/percolator-cmp}}]
        [:cmd/route {:from :tc/percolator-cmp :to :tc/interop-cmp}]
        [:cmd/route {:from :tc/scheduler-cmp :to :tc/client-cmp}]
 
@@ -51,9 +55,10 @@
                           :repeat  true}]}]])))
 
 (defn -main
-  "Starts the application from command line. Also saves and logs process ID. The system that is fired up when
-  start! is called proceeds in core.async's thread pool. Since we don't want the application to exit when
-  just because the current thread is out of work, we just put it to sleep."
+  "Starts the application from command line. Also saves and logs process ID.
+   The system that is fired up when start! is called proceeds in core.async's
+   thread pool. Since we don't want the application to exit when just because
+   the current thread is out of work, we just put it to sleep."
   [& args]
   (pid/save (:pidfile-name conf))
   (pid/delete-on-shutdown! (:pidfile-name conf))

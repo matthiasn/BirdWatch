c678e121b3adc5b54c1102a6e9e5901b87bb783e
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2016-10-29 15:28:51 +0200

    latest Clojure 1.9 alpha; fixed http client

--- Clojure-Websockets/TwitterClient/project.clj
+++ Clojure-Websockets/TwitterClient/project.clj
@@ -2,10 +2,11 @@
   :description "Twitter client part of the BirdWatch system"
   :url "https://github.com/matthiasn/Birdwatch"
   :license {:name "GNU General Public License" :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha10"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
                  [clj-http "3.3.0"]
                  [org.clojure/data.json "0.2.6"]
-                 [twitter-api "0.7.8" :exclusions [org.clojure/data.json]]
+                 [matthiasn/http.async.client "0.5.4"]
+                 [twitter-api "0.7.8" :exclusions [http.async.client]]
                  [org.clojure/core.match "0.3.0-alpha4"]
                  [clojurewerkz/elastisch "2.2.2"]
                  [org.clojure/tools.logging "0.3.1"]

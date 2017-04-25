fedc031e06dbd93bd0ba3dfb418ee2fd98e5dc06
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2017-04-25 13:52:05 +0200

    systems-toolbox-ui removed; updated deps

--- Clojure-Websockets/TwitterClient/project.clj
+++ Clojure-Websockets/TwitterClient/project.clj
@@ -3,21 +3,21 @@
   :url "https://github.com/matthiasn/Birdwatch"
   :license {:name "GNU General Public License" :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha15"]
-                 [clj-http "3.4.1"]
+                 [clj-http "3.5.0"]
                  [org.clojure/data.json "0.2.6"]
                  [matthiasn/http.async.client "0.5.4"]
                  [twitter-api "0.7.8" :exclusions [http.async.client]]
                  [org.clojure/core.match "0.3.0-alpha4"]
                  [clojurewerkz/elastisch "2.2.2"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [matthiasn/systems-toolbox "0.6.6"]
+                 [matthiasn/systems-toolbox "0.6.7"]
                  [matthiasn/systems-toolbox-redis "0.6.3"]
                  [matthiasn/birdwatch-specs "0.3.1"]
                  [matthiasn/systemd-watchdog "0.1.3"]
-                 [ch.qos.logback/logback-classic "1.2.2"]
+                 [ch.qos.logback/logback-classic "1.2.3"]
                  [clj-time "0.13.0"]
                  [clj-pid "0.1.2"]
-                 [com.taoensso/carmine "2.15.1"]]
+                 [com.taoensso/carmine "2.16.0"]]
   :source-paths ["src/clj/"]
   :main ^:skip-aot birdwatch-tc.main
   :target-path "target/%s"
